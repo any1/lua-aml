69d0ede8977f11827503506be42aa1655b0a8225
Andri Yngvason <andri@yngvason.is>
2021-10-21 21:05:54 +0000

    plugins: async: Return promise

--- plugins/async.lua
+++ plugins/async.lua
@@ -1,14 +1,20 @@
+local promise = require 'promise'
+
 function async(cb)
-	local co = coroutine.create(cb)
-
-	local function await(prom)
-		prom:thus(function(result)
-			coroutine.resume(co, result)
+	return promise(function(resolve)
+		local co = coroutine.create(function(...)
+			resolve(cb(...))
 		end)
-		return coroutine.yield()
-	end
 
-	coroutine.resume(co, await)
+		local function await(prom)
+			prom:thus(function(result)
+				coroutine.resume(co, result)
+			end)
+			return coroutine.yield()
+		end
+
+		coroutine.resume(co, await)
+	end)
 end
 
 return async
